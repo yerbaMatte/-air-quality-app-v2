94536d7fe071173a3ef6673d5f2e5ecbc63bc109
Milosz Lewandowski <milosz.lewandowski@icloud.com>
2023-02-23 15:44:28 +0100

    get tiles

--- src/components/Hooks/DataFetcher.tsx
+++ src/components/Hooks/DataFetcher.tsx
@@ -2,24 +2,17 @@ import React from 'react';
 import { useQueryClient, useQuery } from 'react-query';
 import axios from 'axios';
 import { convertLatLngToTiles } from '../HelperFunctions/convertLatLng';
-const DataFetcher = () => {
+
+const DataFetcher = (southWest: number[], northEast: number[]) => {
   // const warsawTiles = convertLatLngToTiles(52.237049, 21.017532, 10);
 
-  // const fetchAxios = async (data: string) => {
-  //   const response = await axios.get(data);
-  //   const quizData = response.data;
-  //   return quizData;
-  // };
-
   const { data, isSuccess } = useQuery('AirData', async () => {
-    const response = await axios.get(
-      'https://tiles.waqi.info/tiles/usepa-aqi/10/571/337.png?token=b4a90c69db861da6fd209f61dcbf5d8f1b874f57'
-    );
+    const response = await axios.get(`
+    https://api.waqi.info/v2/map/bounds?latlng=${southWest},${northEast}&networks=all&token=b4a90c69db861da6fd209f61dcbf5d8f1b874f57`);
     const quizData = response.data;
+    console.log(quizData);
     return quizData;
   });
-
-  console.log(data);
 
   return <div>DataFetcher</div>;
 };
@@ -28,5 +21,3 @@ export default DataFetcher;
 
 // token
 // b4a90c69db861da6fd209f61dcbf5d8f1b874f57
-
-// https://tiles.waqi.info/tiles/usepa-aqi/10/571/337.png?token=b4a90c69db861da6fd209f61dcbf5d8f1b874f57
--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,23 +1,34 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import './Map.css';
 import DataFetecher from '../Hooks/DataFetcher';
+
 export const Map = () => {
+  function GetTiles() {
+    const map = useMap();
+    const bounds = map.getBounds();
+    let southWest = [bounds._southWest.lat, bounds._southWest.lng];
+    let northEast = [bounds._northEast.lat, bounds._northEast.lng];
+    console.log(southWest, northEast);
+    DataFetecher(southWest, northEast);
+    return (
+      <>
+        <Marker position={[52.19908510453474, 20.94423294067383]} />
+        <Marker position={[52.274880130680536, 21.090660095214847]} />
+      </>
+    );
+  }
+
   return (
     <MapContainer
       center={[52.237049, 21.017532]}
       zoom={11}
       scrollWheelZoom={true}
     >
+      <GetTiles />
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
-      <Marker position={[52.237049, 21.017532]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-      <DataFetecher />
     </MapContainer>
   );
 };
