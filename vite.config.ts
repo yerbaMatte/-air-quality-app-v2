f14ebe8d0708133f71939e9116d340e119f6e47c
Milosz Lewandowski <milosz.lewandowski@icloud.com>
2023-02-26 12:34:37 +0100

    change base path - fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+//change the base path
 export default defineConfig({
   plugins: [react()],
   base: '/-air-quality-app-v2/',
